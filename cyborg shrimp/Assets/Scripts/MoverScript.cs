1579d545dc2756bd47807e12d60ca8291c414a26
Taylor Manning <10710262@my.uvu.edu>
2020-09-02 19:56:29 -0600

    MoverScript stuff

--- cyborg shrimp/Assets/Scripts/MoverScript.cs
+++ cyborg shrimp/Assets/Scripts/MoverScript.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
+    public float speed = 3f;
+    public float x, y, z;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.log(message: "hello World");
+        Debug.Log("weeeee");
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = speed*Input.GetAxis("Horizontal") * Time.deltaTime;
+        y = speed*Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(x,y,z);
     }
 }
