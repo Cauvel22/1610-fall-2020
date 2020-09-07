959351b6f7d74929c6b01db3e381170fbdacf0df
Taylor Manning <10710262@my.uvu.edu>
2020-09-06 20:45:56 -0600

    BillyAndTime

--- cyborg shrimp/Assets/Scripts/MoverScript.cs
+++ cyborg shrimp/Assets/Scripts/MoverScript.cs
@@ -10,17 +10,16 @@ public class MoverScript : MonoBehaviour
     public float speed = 3f, x, y, z;
     public float health = 1;
     public int point = 10;
-
-
+    public string password = "Quite foggy today huh?";
     private void Start()
     {
-        Debug.Log("My name's Lil'Billy!")
+        Debug.Log("My name's Lil'Billy!");
     }
 
     private void Update()
     {
-        x = speed *Time.deltaTime * Input.GetAxis("Vertical");
-        y = speed *Time.deltaTime * Input.GetAxis("Horizontal");
+        x = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        y = speed * Time.deltaTime * Input.GetAxis("Vertical");
         transform.Translate(x,y,z);
     }
 
