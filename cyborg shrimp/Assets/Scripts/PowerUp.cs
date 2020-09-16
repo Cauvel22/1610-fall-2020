879141f14558f84d668e9b004c84fc62369eb9ed
Taylor Manning <10710262@my.uvu.edu>
2020-09-15 20:20:58 -0600

    SomeStuff

--- cyborg shrimp/Assets/Scripts/PowerUp.cs
+++ cyborg shrimp/Assets/Scripts/PowerUp.cs
@@ -1,11 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public Vector3 RotationVector3;
+    public float speed = 10f;
     private void OnTriggerEnter(Collider other)
     {
        score++;
        print(score);
+    }
+
+    private void Update()
+    {
+        RotationVector3.y = speed * Time.deltaTime;
+        transform.Rotate(RotationVector3);
     }
 }
