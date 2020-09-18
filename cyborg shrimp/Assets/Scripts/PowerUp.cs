368165adb54c26394c5846b600b4a2280f2ff3cf
Taylor Manning <10710262@my.uvu.edu>
2020-09-17 20:10:40 -0600

    cleaning

--- cyborg shrimp/Assets/Scripts/PowerUp.cs
+++ cyborg shrimp/Assets/Scripts/PowerUp.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
     public Vector3 RotationVector3;
     public float speed = 10f;
+    public UnityEvent powerUpEvent
     private void OnTriggerEnter(Collider other)
     {
        score++;
@@ -17,5 +19,6 @@ public class PowerUp : MonoBehaviour
     {
         RotationVector3.y = speed * Time.deltaTime;
         transform.Rotate(RotationVector3);
+        //UnityEvent.invoke;
     }
 }
--- learningWithCode1/Assets/Scripts/PlayerController.cs
+++ learningWithCode1/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //private variables
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*20);
+        //player movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //player turn
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
