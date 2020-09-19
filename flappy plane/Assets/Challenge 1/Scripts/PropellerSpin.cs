9d1085dfb799d3962f9d4fbc7852ea92d7be82cf
Taylor Manning <10710262@my.uvu.edu>
2020-09-18 21:04:05 -0600

    plane workshop

---
+++ flappy plane/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+
+
+{
+    public float speed;
+    public Vector3 rotationDirection;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward *3f);
+    }
+}
