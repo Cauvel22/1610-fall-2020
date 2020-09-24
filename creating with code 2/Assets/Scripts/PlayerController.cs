fd11377ad7db9d46d071ac0bfdf8e402a663302b
Taylor Manning <10710262@my.uvu.edu>
2020-09-23 22:22:33 -0600

    heck

---
+++ creating with code 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+
+{
+   public float horizontalInput;
+   public float speed = 3f;
+   private void Update()
+   {
+      horizontalInput = Input.GetAxis("Horizontal") *speed*Time.deltaTime;
+   }
+}
