da70e9ba3d8aecf158452e3d3b3971f7140c3023
Taylor Manning <10710262@my.uvu.edu>
2020-09-24 17:57:55 -0600

    Shooting Steaks

---
+++ creating with code 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 40;
+
+
+    public void Update()
+    {
+        transform.Translate(Vector3.forward*speed*Time.deltaTime);
+
+         
+    }
+}
--- creating with code 2/Assets/Scripts/PlayerController.cs
+++ creating with code 2/Assets/Scripts/PlayerController.cs
@@ -8,9 +8,28 @@ public class PlayerController : MonoBehaviour
 
 {
    public float horizontalInput;
-   public float speed = 3f;
+   public float speed = 10.0f;
+   public float xRange = 15f;
+   public GameObject projectilePrefab;
    private void Update()
    {
-      horizontalInput = Input.GetAxis("Horizontal") *speed*Time.deltaTime;
+      if (transform.position.x < -xRange)
+      {
+         transform.position = new Vector3(-xRange,transform.position.y,transform.position.z);
+      }
+
+      if (transform.position.x > xRange)
+      {
+         transform.position=new Vector3(xRange,transform.position.y,transform.position.z);
+      }
+      
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+      }
+
+      
+      horizontalInput = Input.GetAxis("Horizontal");
+      transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
    }
 }
