935062fb13a0b274be11f89b2f52b192119f97b3
Taylor Manning <10710262@my.uvu.edu>
2020-09-25 20:29:33 -0600

    RandomSpacing

---
+++ creating with code 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    Random number = new Random();
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[number.Next(0,37) % 4], new Vector3((float)number.Next(-20, 
+            20),0,25), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
