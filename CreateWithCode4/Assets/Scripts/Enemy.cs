905823c1d027a1d532d20a3b0961d49bdbf0d3d2
Taylor Manning <10710262@my.uvu.edu>
2020-10-24 14:00:38 -0600

    finished ball game

--- CreateWithCode4/Assets/Scripts/Enemy.cs
+++ CreateWithCode4/Assets/Scripts/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
     public float speed = 3f;
     private Rigidbody enemyRB;
     private GameObject player;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,11 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection =
             (player.transform.position - transform.position).normalized;
         enemyRB.AddForce(lookDirection * speed);
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
+        
     }
 }
--- CreateWithCode4/Assets/Scripts/SpawnManagerScript.cs
+++ CreateWithCode4/Assets/Scripts/SpawnManagerScript.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class SpawnManagerScript : MonoBehaviour
 {
     public GameObject enemyPrefab;
-
+    public int enemyCount;
     public float spawnRange = 9;
+    public int waveNumber = 1;
+    public GameObject powerUpPrefeb;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerUpPrefeb, GenerateSpawnPos(), powerUpPrefeb.transform.rotation);
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerUpPrefeb, GenerateSpawnPos(), powerUpPrefeb.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+        
     }
 
     private Vector3 GenerateSpawnPos()
